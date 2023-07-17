5d78bb1250a5fdba2d289fa3c05c822096d49b00
jack12347410 <jack12347410@gmail.com>
2023-07-17 16:33:17 +0800

    docker

--- WebMvc/Controllers/AccountController.cs
+++ WebMvc/Controllers/AccountController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace WebMvc.Controllers
 {
+    [Authorize]
     public class AccountController:Controller
     {
+        public async Task<IActionResult> LogIn(string returnUrl)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var idToken = await HttpContext.GetTokenAsync("id_token");
+
+            foreach(var claim in User.Claims)
+            {
+                Debug.WriteLine($"Claim Type: {claim.Type} - Claim Value : {claim.Value}");
+            }
+
+            if (accessToken != null) ViewData["access_token"] = accessToken;
+            if (idToken != null) ViewData["id_token"] = idToken;
+
+            return RedirectToAction(nameof(CatalogController.About), "Catalog");
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+
+            var homeUrl = Url.Action(nameof(CatalogController.Index), "Catalog");
+
+            return new SignOutResult(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = homeUrl });
+        }
     }
 }
